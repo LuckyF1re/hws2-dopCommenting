d26f898ba5e80d197b70c380fa575126aa57df57
LuckyF1re <andruha.sadovich@gmail.com>
2025-04-24 19:18:08 +0300

    sgdgs

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,24 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            const newState = [...state]
+
+                if (action.payload === 'up') {
+                    newState.sort((a,b) => a.name.localeCompare(b.name))
+                } else {
+                    newState.sort((a,b) => b.name.localeCompare(a.name))
+                }
+
+                return  newState
+            // need to fix
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(s => s.age >= 18) // need to fix
         }
         default:
             return state
