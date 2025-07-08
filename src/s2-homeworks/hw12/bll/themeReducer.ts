cd28c51d5a1d6a319a225f361e896ecd7bd56fe2
LuckyF1re <andruha.sadovich@gmail.com>
2025-07-08 20:25:26 +0300

    FUBAR

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,24 @@
-const initState = {
+type ThemeState = {
+    themeId: number
+}
+
+const initState: ThemeState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ThemeAction): ThemeState => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ThemeAction => ({ type: 'SET_THEME_ID' as const, id }) // fix any
+
+type ThemeAction = {
+    type: string
+    id: number
+}
